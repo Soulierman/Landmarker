8fc8d8ae7b5534a138e174084a33f4876d025a16
Soufiane Salmane <salm05soufiane@gmail.com>
2026-03-14 18:14:49 -0400

    Add template and route for adding locations

--- app.py
+++ app.py
@@ -8,3 +8,6 @@ app.secret_key = secrets.token_hex(16)
 def home():
     return render_template('index.html')
 
+@app.route("/addlocation")
+def add_location():
+    return render_template('location_form.html')
