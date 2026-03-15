0952720d1e4a593bc68271a2c7e4f674ab6a84fc
Soufiane Salmane <salm05soufiane@gmail.com>
2026-03-15 17:44:20 -0400

    Add db initializer and create empty db

---
+++ dbinit.py
@@ -0,0 +1,29 @@
+import sqlite3
+
+conn = sqlite3.connect('landmarker.db')
+
+conn.execute('''CREATE TABLE Users
+(ID INTEGER PRIMARY KEY AUTOINCREMENT,
+username    VARCHAR(255)    NOT NULL,
+password        VARCHAR(255)    NOT NULL);''')
+print("Users table created successfully")
+
+conn.execute('''CREATE TABLE Locations
+(ID INTEGER PRIMARY KEY AUTOINCREMENT,
+userID INTEGER NOT NULL REFERENCES Users(ID),
+latitude REAL NOT NULL,
+longitude REAL NOT NULL,
+rating REAL);''')
+print("Locations table created successfully")
+
+conn.execute('''CREATE TABLE Pictures
+(ID INTEGER PRIMARY KEY AUTOINCREMENT,
+locationID INTEGER NOT NULL REFERENCES Users(ID),
+imagename VARCHAR(255) NOT NULL);''')
+print("Pictures table created successfully")
+
+conn.execute('''INSERT INTO Users (username,password) VALUES ("Alice",12345)''')
+conn.execute('''INSERT INTO Users (username,password) VALUES ("Bob",56789)''')
+conn.commit()
+
+# INTEGER PRIMARY KEY AUTOINCREMENT
